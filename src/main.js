c79c359056a6254c25862b087213c8e8266ef415
JMaylor <joseph.maylor@gmail.com>
2020-11-01 20:24:01 +0000

    primevue config

--- src/main.js
+++ src/main.js
@@ -2,23 +2,26 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import "primeflex/primeflex.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
-import "primevue/resources/themes/vela-orange/theme.css";
+import "primevue/resources/themes/saga-green/theme.css";
 
-import Dialog from "primevue/dialog";
 import Button from "primevue/button";
-import Menubar from 'primevue/menubar';
-import InputText from 'primevue/inputtext'
+import Menubar from "primevue/menubar";
+import InputText from "primevue/inputtext";
+import ProgressBar from "primevue/progressbar";
 
 const app = createApp(App)
     .use(store)
     .use(router);
 
-app.component("Dialog", Dialog);
+app.config.globalProperties.$primevue = { ripple: true };
+
 app.component("Button", Button);
 app.component("Menubar", Menubar);
 app.component("InputText", InputText);
+app.component("ProgressBar", ProgressBar);
 
 app.mount("#app");
