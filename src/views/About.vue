4a1c0ce90bdcd99fd520e25fc00dfe3b256450cc
JMaylor <joseph.maylor@gmail.com>
2020-11-02 08:26:19 +0000

    prettify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>This is a simple game made in VueJS</h1>
-    <a href="https://github.com/JMaylor/typing-game" target="_blank"><i class="pi pi-github" style="fontSize: 2rem"></i></a>
+    <a href="https://github.com/JMaylor/typing-game" target="_blank"
+      ><i class="pi pi-github" style="fontSize: 2rem"></i
+    ></a>
   </div>
 </template>
